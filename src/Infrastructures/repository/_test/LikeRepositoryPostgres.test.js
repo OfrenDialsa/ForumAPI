6e9e4766326432d1fc72c67667a9957f91bf8a98
OfrenDialsa <ofrendialsa.dev@gmail.com>
2025-12-05 21:41:31 +0700

    fix: LikeRepository

--- src/Infrastructures/repository/_test/LikeRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/LikeRepositoryPostgres.test.js
@@ -26,7 +26,7 @@ describe('LikeRepositoryPostgres',()=>{
     });
 
     describe('add like function', ()=>{
-        it('should add like correctly (FAIL VERSION)', async()=>{
+        it('should add like correctly', async()=>{
             const thread_id = 'thread-123';
             const comment_id= 'comment-123';
             const owner= 'user-123';
@@ -38,41 +38,43 @@ describe('LikeRepositoryPostgres',()=>{
 
             const result = await LikesTableTestHelper.findLike('like-123');
             
-            // DIPAKSA FAIL
-            expect(result).toBeFalsy();
+            expect(result).toBeTruthy();
         });
     });
 
     describe('get like function', ()=>{
-        it('should return 0 when comment is not liked (FAIL VERSION)', async ()=>{
+        it('should return 0 when comment is not liked by user',async ()=>{
+            
             const comment_id= 'comment-123';
             const owner= 'user-123';
 
-            const likeRepository = new LikeRepositoryPostgres(pool, ()=>'123');
+            const fakeIdGenerator = () => '123';
+            const likeRepository = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+    
             const result = await likeRepository.getLike(comment_id, owner);
             
-            // DIPAKSA FAIL
-            expect(result).toBeTruthy();
+            expect(result).not.toBeTruthy();
         });
 
-        it('should not return 0 when comment is liked (FAIL VERSION)', async()=>{
+        it('should not return 0 when comment is liked by user', async()=>{
             const thread_id = 'thread-123';
             const comment_id= 'comment-123';
             const owner= 'user-123';
 
-            const likeRepository = new LikeRepositoryPostgres(pool, ()=>'123');
+            const fakeIdGenerator = () => '123';
+            const likeRepository = new LikeRepositoryPostgres(pool, fakeIdGenerator);
     
             await likeRepository.addLike(new NewLike({comment_id,owner,thread_id}));
             const result = await likeRepository.getLike(comment_id, owner);
             
-            // DIPAKSA FAIL
-            expect(result).toBeFalsy();
+            expect(result).toBeTruthy();
         });
     });
 
     
     describe('delete like function', ()=>{
-        it('should delete like from table (FAIL VERSION)', async () => {
+        it('should delete like from table', async () => {
+
             const comment_id = 'comment-123';
             const owner = 'user-123';
 
@@ -81,19 +83,17 @@ describe('LikeRepositoryPostgres',()=>{
             const likeRepository = new LikeRepositoryPostgres(pool, {});
             
             const find = await likeRepository.getLike(comment_id, owner);
-            // DIPAKSA FAIL
-            expect(find).not.toBeTruthy();
+            expect(find).toBeTruthy();
 
             await likeRepository.deleteLike(comment_id, owner);
 
             const find2 = await likeRepository.getLike(comment_id, owner);
-            // DIPAKSA FAIL
-            expect(find2).toBeTruthy();
+            expect(find2).not.toBeTruthy();
         });
     });
 
     describe('get like count function', ()=>{
-        it('should return every liked comment in thread (FAIL VERSION)', async () => {
+        it('should return every liked comment in thread', async () => {
             const thread_id= 'thread-123';
     
             await LikesTableTestHelper.addLike({ id: 'like-123', comment_id: 'comment-123', owner: 'user-123' });
@@ -104,9 +104,16 @@ describe('LikeRepositoryPostgres',()=>{
     
             const likeCount = await likeRepository.getLikeCount(thread_id);
     
-            // DIPAKSA FAIL (array salah total)
             expect(likeCount).toStrictEqual([
-              { comment_id: 'xxx' },
+              {
+                comment_id: 'comment-123',
+              },
+              {
+                comment_id: 'comment-123',
+              },
+              {
+                comment_id: 'comment-124',
+              },
             ]);
         });
     });
